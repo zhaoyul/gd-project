b5325f3f6947ab27283c132929c160bc50ef2141
Kevin.Li <li_zhaoyu@msn.com>
2018-03-17 09:35:44 +0800

    增加db query

---
+++ src/clj/gd_backend/db/core.clj
@@ -0,0 +1,12 @@
+(ns guestbook.db.core
+  (:require
+   [clj-time.jdbc]
+   [conman.core :as conman]
+   [mount.core :refer [defstate]]
+   [gd-backend.config :refer [env]]))
+
+(defstate ^:dynamic *db*
+  :start (conman/connect! {:jdbc-url (env :database-url)})
+  :stop (conman/disconnect! *db*))
+
+(conman/bind-connection *db* "sql/queries.sql")
